9f82ec8305c49b58ffac274a768d022ca236e6f6
david <dvdm36@gmail.com>
2025-07-06 03:57:01 +0000

    It works! need to build rust with wasm-pack build --target web

--- cgol-web/src/app/app.ts
+++ cgol-web/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import init, { greet } from '../../../cgol-rust/pkg'
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  ngOnInit(): void {
+    init();
+  }
+
   protected title = 'cgol-web';
+
+  onGreetClick() {
+    greet();
+  }
 }
